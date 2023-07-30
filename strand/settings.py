7ef3baa334ce6f6bc2cde595d5c5c3b224b8582b
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-07-30 19:01:37 +0000

    commit procfile and deployment

--- strand/settings.py
+++ strand/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -29,7 +29,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-anthonyhaj-strandpp4-8o0i5h4tuly.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = [
+    '8000-anthonyhaj-strandpp4-8o0i5h4tuly.ws-eu102.gitpod.io', 
+    'strand-pp4.herokuapp.com',
+    'localhost',
+]
 
 
 # Application definition
@@ -63,7 +67,7 @@ ROOT_URLCONF = 'strand.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
