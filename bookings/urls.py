57814f986e0105b02ae37d381e4033c5301fe840
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-07-31 18:16:06 +0000

    add fix for url redirect for bookings.html

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'bookings'
+
 urlpatterns = [
-    path('book/', views.book, name='bookings'),
+    path('', views.book, name='bookings'),
 ]
