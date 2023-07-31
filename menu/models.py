03df5bbf58374304f09288724ed1f8d8377b0d2c
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-07-31 13:50:48 +0000

    add food and drink items to models.py

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+FOOD_TYPES = (
+    (1, 'Appetizer'),
+    (2, 'Main Course'),
+    (3, 'Dessert'),
+)
+
+DRINK_TYPES = (
+    (1, 'Soft Drink'),
+    (2, 'Wine'),
+    (3, 'Beer'),
+)
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    price = models.FloatField()
+    type = models.IntegerField(choices=FOOD_TYPES)
+
+    def __str__(self):
+        return self.name
+
+class DrinkItem(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    price = models.FloatField()
+    type = models.IntegerField(choices=DRINK_TYPES)
+
+    def __str__(self):
+        return self.name
