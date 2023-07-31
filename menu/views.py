74d057b26052337f0fc52b3845d4e01fe8a5f32b
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-07-31 14:30:51 +0000

    add views for fooditem and drinkitem + url pattern

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import FoodItem, DrinkItem
 
-# Create your views here.
+
+def menu(request):
+    food_items = FoodItem.objects.all()
+    drink_items = DrinkItem.objects.all()
+    context = {
+        'food_items': food_items,
+        'drink_items': drink_items,
+    }
+    return render(request, 'menu.html', context)
