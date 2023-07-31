983d31f1d20a347986f01a21521a8d5300fa67b2
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-07-31 17:49:31 +0000

    connect bookings page with base.html and corresponding urlpatterns for bookings

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def book(request):
+    return render(request, 'bookings.html')
--- strand/urls.py
+++ strand/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', include('menu.urls')),
     path('home/', include('home.urls')),
+    path('bookings/', include('bookings.urls')),
 ]
