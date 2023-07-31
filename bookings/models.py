ecc720462c14e077315c1c4ff63b1a8a65dd43c5
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-07-31 16:43:26 +0000

    add restaurant table class for booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class RestaurantTable(models.Model):
+    name = models.CharField(max_length=50)
+    max_seats = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
