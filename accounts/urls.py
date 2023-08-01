a2897bad438979cb3d4c59b4efeaca119610cfda
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-08-01 17:38:06 +0000

    add user auth for signed in user in base.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('register/success/', views.register_success, name='success'),
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -35,4 +35,4 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect('home.html')
+    return redirect('home')
