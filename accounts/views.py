d56d723a5b5b04afc5ecaef4fcfc7445367290ae
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-08-01 16:01:20 +0000

    add success html structure and message success

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-
+from django.contrib import messages
 
 def register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home.html')
+            messages.success(request, 'Registration successful. You can now log in to your account.')
+            return redirect('accounts:success') 
     else:
         form = RegisterForm()
     return render(request, 'register.html', {'form': form})
+
+def register_success(request):
+    return render(request, 'success.html')
+
