60bbaf39f540cb3edb2055da2c8bf3ba7139f445
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-08-01 14:51:04 +0000

    add url patterns for accounts in app and proj urls.py

--- strand/urls.py
+++ strand/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('menu/', include('menu.urls')),
     path('home/', include('home.urls')),
     path('bookings/', include('bookings.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
