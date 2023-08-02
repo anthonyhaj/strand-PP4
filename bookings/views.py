9738d9ae6a563527166be52acd6060db8a987bc4
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-08-02 12:21:05 +0000

    add def book in views for bookings form

--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookingForm
 
 def book(request):
-    return render(request, 'bookings.html')
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.guest = request.user
+            booking.save()
+            return redirect('bookings: my_bookings.html')
+    else:
+        form = BookingForm()
+
+    return render(request, 'bookings/bookings.html', {'form': form})
