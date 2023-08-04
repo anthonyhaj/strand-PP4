17e5c9d6e76d49d1c2fa67d3166a21cbcec6ac8d
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-08-04 17:02:10 +0000

    add structure a bootstrap styling to contact.html and modify contact views

--- contact/views.py
+++ contact/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .forms import ContactForm
 from django.contrib.auth.decorators import login_required
 
-@login_required
+
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
