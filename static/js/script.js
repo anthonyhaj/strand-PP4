deb71a1ff1bdf80835b2644043373685e29cec64
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-08-04 15:16:41 +0000

    add script for book a table date picker to avoid past date selection

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const dateField = document.querySelector('#id_requested_date');
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+
+    dateField.min = yyyy + '-' + mm + '-' + dd;
+});
