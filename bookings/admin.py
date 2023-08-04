8d954e66bb10fe8599cae233f15463257172bb45
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-08-04 14:46:30 +0000

    modify bookings admin.py for list display in admin interface

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Booking, RestaurantTable
 
-admin.site.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_guest_full_name', 'guest_count', 'status')
+
+    def get_guest_full_name(self, obj):
+        return f"{obj.guest.first_name} {obj.guest.last_name}"
+    get_guest_full_name.short_description = 'Guest Name'  
+
+admin.site.register(Booking, BookingAdmin)
 admin.site.register(RestaurantTable)
+
