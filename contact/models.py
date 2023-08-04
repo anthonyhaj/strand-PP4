f54d298db290da5d7bc40108f32d1a9ef21f8ecd
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-08-04 18:21:55 +0000

    add message textfield to contact model

--- contact/models.py
+++ contact/models.py
@@ -5,6 +5,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     phone = models.CharField(max_length=50)
+    message = models.TextField(null=True)
 
     def __str__(self):
         return self.name
