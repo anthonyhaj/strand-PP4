1b328859af1ad5ff9cf81c0487579ae3f1da5039
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-08-05 12:25:20 +0000

    fix heroku deploy issue

--- strand/settings.py
+++ strand/settings.py
@@ -15,7 +15,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['strand-pp4-d640c87ff185.herokuapp.com', '8000-anthonyhaj-strandpp4-8o0i5h4tuly.ws-eu102.gitpod.io', 'localhost']
 
